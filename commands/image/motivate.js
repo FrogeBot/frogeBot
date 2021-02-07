0348ae74ae8dd3cd92a70299381a39e43d174cc5
Barnaby <barnab.why@gmail.com>
2021-02-07 19:14:50 +0000

    Update motivate.js

--- commands/image/motivate.js
+++ commands/image/motivate.js
@@ -21,6 +21,7 @@ async function cmdFunc(msg, args) {
         let width = Math.round(height * 4/3)
         
         let scaleFactor = 0.75;
+        if(height < 2000 && width < 2000) scaleFactor = 1;
 
         let rectCanvas = await canvasRect(Math.round(imgFG.bitmap.width*1.05), Math.round(imgFG.bitmap.height+imgFG.bitmap.width*0.05), "white", Math.round(imgFG.bitmap.width*0.005), "black")
 
@@ -28,7 +29,7 @@ async function cmdFunc(msg, args) {
 
         let textCanvas2 = await canvasText(args.split("|").slice(1).join("|").trim(), Math.round(width*0.05*scaleFactor), "Times New Roman", Math.round(width*0.9*scaleFactor), "center", 1.5, "white")
 
-        let offset = textCanvas[1]/scaleFactor+textCanvas2[1]/scaleFactor+Math.round(imgFG.bitmap.width*0.075);
+        let offsetScaled = textCanvas[1]/scaleFactor+textCanvas2[1]/scaleFactor+Math.round(imgFG.bitmap.width*0.075);
 
         let img = await execNewImage(width, height+offset, "#000000", [
             ["composite", [rectCanvas, Math.round(width/2-imgFG.bitmap.width/2 - Math.round(imgFG.bitmap.width*0.025)), Math.round(imgFG.bitmap.height*0.05) - Math.round(imgFG.bitmap.width*0.025)]],

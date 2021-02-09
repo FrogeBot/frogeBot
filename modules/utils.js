8d028b153ecb1ebb6e39107a934e5d507135e9c2
Barnaby <barnab.why@gmail.com>
2021-02-09 11:15:32 +0000

    Add gifv embed support (tenor)

--- modules/utils.js
+++ modules/utils.js
@@ -17,6 +17,9 @@ function findImage(msg) {
                         if(message.embeds[0] && message.embeds[0].type == "image") {
                             attachmentURL = message.embeds[0].url // If message has image embed set as URL
                         }
+                        if(message.embeds[0] && message.embeds[0].type == "gifv") {
+                            attachmentURL = message.embeds[0].url+(message.embeds[0].url.match(/(\.gif)/gi) ? "" : ".gif") // If message has gifv embed set as URL (Ensuring it ends with .gif)
+                        }
                     }
                     if(attachmentURL) return attachmentURL // Return image URL for each message
                 }).filter(a => a != undefined); // Filter out messages with no image

02cc65ea3d9f49d4520d4ff37d8de53f91234bd8
Barnaby <barnab.why@gmail.com>
2021-02-10 16:04:13 +0000

    Remove obsolete GIF warning

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -9,7 +9,7 @@ function cmdFunc(msg, args) {
     msg.channel.send({
         "embed": {
             "title": "Help",
-            "description": Object.keys(cmdHelpList).slice((page-1)*12, page*12).map(key => { return "**"+key+"**" + ' ' + cmdHelpList[key] }).join('\n')+`\n\n*GIF images are restricted to the first ${process.env.GIF_FRAME_LIM} frames.*`,
+            "description": Object.keys(cmdHelpList).slice((page-1)*12, page*12).map(key => { return "**"+key+"**" + ' ' + cmdHelpList[key] }).join('\n'),
             "color": Number(process.env.EMBED_COLOUR),
             "footer": {
               "text": `Page ${page} of ${Math.ceil(Object.keys(cmdHelpList).length/12)}`

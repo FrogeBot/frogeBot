695977a497b95362b62340c9392c2705ff3d9016
Barnaby <barnab.why@gmail.com>
2021-02-12 13:05:22 +0000

    MMMMMM CAT!!!!!!

---
+++ commands/misc/cat.js
@@ -0,0 +1,48 @@
+require("dotenv").config()
+
+const request = require("request")
+
+async function cmdFunc(msg, args, startTime) {
+    let catUrl = `https://api.thecatapi.com/v1/images/search`
+    request(catUrl, function(error, response, body){
+        if (error)
+            msg.channel.send({
+                embed: {
+                    "title": "Error",
+                    "description": `<@${msg.author.id}> - ${process.env.MSG_SEND_FAIL}`,
+                    "color": Number(process.env.EMBED_COLOUR),
+                    "timestamp": new Date(),
+                    "author": {
+                        "name": process.env.BOT_NAME,
+                        "icon_url": msg.client.user.displayAvatarURL()
+                    }
+                }
+            })
+        else {
+            let catJSON = JSON.parse(response.body)
+            
+            msg.channel.send({
+                embed: {
+                    "title": "Cat",
+                    "description": `<@${msg.author.id}> ${process.env.MSG_SUCCESS}`,
+                    "color": Number(process.env.EMBED_COLOUR),
+                    "timestamp": new Date(),
+                    "author": {
+                        "name": process.env.BOT_NAME,
+                        "icon_url": msg.client.user.displayAvatarURL()
+                    },
+                    "footer": {
+                        "text": "mmmmmm cat"
+                    },
+                    "image": {
+                        "url": catJSON[0].url
+                    }
+                }
+            })
+        }
+    });
+}
+
+module.exports = {
+    cmdFunc
+}

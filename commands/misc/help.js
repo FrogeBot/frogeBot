26bfcbc325d660ec1dd8ebe384da3ab181982d8c
Barnaby <barnab.why@gmail.com>
2021-02-13 13:02:51 +0000

    Improved help command
    Split by category

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,18 +1,48 @@
 require("dotenv").config()
+const fs = require("fs")
+const YAML = require('yaml')
 
 function cmdFunc(msg, args) {
-    delete require.cache[require.resolve("../helpList.json")];
-    let cmdHelpList = require("../helpList.json")
+    const commands = YAML.parse(fs.readFileSync('./commands.yml', 'utf8'))
 
-    let page = (args.length > 0 && Number.isInteger(Number(args)) && Number(args) >= 1 && Number(args) <= Math.ceil(Object.keys(cmdHelpList).length/12)) ? Number(args) : 1;
+    let maxPerPage = 10;
+
+    let categories = {}
+    let categoriesPaged = {}
+    Object.keys(commands).forEach(cmd => {
+        if(commands[cmd].hidden || commands[cmd].category == undefined || commands[cmd].description == undefined) return
+        if(categories[commands[cmd].category] == undefined) categories[commands[cmd].category] = []
+        categories[commands[cmd].category].push(cmd)
+    });
+    Object.keys(categories).forEach(cat => {
+        let category = [[]];
+        categories[cat].forEach(cmd => {
+            let page = category.length-1;
+            if(category[page].length >= maxPerPage) {
+                page++;
+                category.push([])
+            }
+            category[page].push(cmd)
+        })
+        categories[cat] = category
+    })
+    
+    let pages = []
+    Object.keys(categories).forEach(cat => {
+        categories[cat].forEach(p => {
+            pages.push({ title: cat, cmds: p })
+        })
+    })
+
+    let page = (args.length > 0 && Number.isInteger(Number(args)) && Number(args) >= 1 && Number(args) <= pages.length) ? Number(args) : 1;
 
     msg.channel.send({
         "embed": {
-            "title": "Help",
-            "description": Object.keys(cmdHelpList).slice((page-1)*12, page*12).map(key => { return "**"+key+"**" + ' ' + cmdHelpList[key] }).join('\n'),
+            "title": `Help - ${pages[page-1].title}`,
+            "description": pages[page-1].cmds.map(key => { return "**"+key+"**" + ' ' + commands[key].description }).join('\n'),
             "color": Number(process.env.EMBED_COLOUR),
             "footer": {
-              "text": `Page ${page} of ${Math.ceil(Object.keys(cmdHelpList).length/12)}`
+              "text": `Page ${page} of ${pages.length}`
             },
             "author": {
                 "name": process.env.BOT_NAME,

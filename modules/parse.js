290780a8f936ca0898410b24f96cb146d1a5552a
Barnaby <barnab.why@gmail.com>
2021-02-15 21:18:21 +0000

    Fix CMD_PREFIX edit bug

--- modules/parse.js
+++ modules/parse.js
@@ -24,7 +24,7 @@ function isCmd(msg) {
 function parseMsg(msg) {
 
     return new Promise((resolve, reject) => {
-        let hasPrefix = msg.content.startsWith("+") || msg.content.startsWith("<@"+msg.client.id+">") // Check if the message has prefix
+        let hasPrefix = msg.content.startsWith(process.env.CMD_PREFIX) || msg.content.startsWith("<@"+msg.client.id+">") // Check if the message has prefix
         if(hasPrefix) {
             let prefix = msg.content.startsWith(process.env.CMD_PREFIX) ? process.env.CMD_PREFIX : (msg.content.startsWith("<@"+msg.client.id+">") ? "<@"+msg.client.id+"> " : null) // Determine which prefix was used
             let cmd = msg.content.substr(prefix.length).split(" ")[0]; // Separate command

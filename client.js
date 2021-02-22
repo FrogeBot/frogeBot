f0f0d95d176362466bc30fb648f50f9169bd38a3
Barnaby <barnab.why@gmail.com>
2021-02-22 22:25:39 +0000

    Add comments to client.js

--- client.js
+++ client.js
@@ -4,27 +4,32 @@ require("dotenv").config() // Get .env
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Log when client is ready
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Catch discord.js errors
 client.on('error', (error) => {
     console.log(error);
 });
 
+// Catch process errors
 process.on('uncaughtException', function (err) {
     console.log(err);
 })
 
-const { parseMsg, isCmd } = require("./modules/parse.js")
+const { parseMsg, isCmd } = require("./modules/parse.js") // Import command parsing functions
 
+// Ready and parse commands.yml
 const fs = require("fs")
 const YAML = require('yaml')
-
 const commands = YAML.parse(fs.readFileSync('./commands.yml', 'utf8'))
 
+// Import image module
 let { exec, execGM, getFormat } = require("@frogebot/image")({ imageMagick: process.env.USE_IMAGEMAGICK, maxGifSize: process.env.MAX_GIF_SIZE, maxImageSize: process.env.MAX_IMAGE_SIZE })
-let { findImage, sendImage } = require("./modules/utils.js")
+
+let { findImage, sendImage } = require("./modules/utils.js") // Import image util commands
 
 client.on('message', async msg => {
     if(msg.author.bot || !await isCmd(msg)) return // If not command or called by bot
@@ -49,21 +54,21 @@ client.on('message', async msg => {
             let r;
             if(cmd.r) { // Handle replacement of input args in "r" val of command
                 for(let i = cmd.r.split('||').length-1; i >= 0; i--) {
-                    newR = cmd.r.split('||')[i].trim()
-                    if(newR.match(/\(input\)/) && args.length > 0) {
-                        newR = newR.replace(/\(input\)/, args)
-                        if(cmd.r_type == 'int' && Number.isInteger(Number(newR))) r = newR
-                        if(cmd.r_type == 'num' && !Number.isNaN(Number(newR))) r = newR
-                    } else if(newR.match(/\(input\:[0-9]+\)/) && args.length > 0) {
-                        newR = newR.replace(/\(input:[0-9]+\)/, args.split(' ')[newR.replace(')','').split(':')[1]])
-                        if(cmd.r_type.startsWith('int') && Number.isInteger(Number(newR))) r = newR
-                        if(cmd.r_type.startsWith('num') && !Number.isNaN(Number(newR))) r = newR
-                    } else if(!newR.match(/\(input\)/) && !newR.match(/\(input\:[0-9]+\)/)) {
-                        r = newR;
+                    newR = cmd.r.split('||')[i].trim() // Separate potential r value
+                    if(newR.match(/\(input\)/) && args.length > 0) { // If r accepts input and args are present
+                        newR = newR.replace(/\(input\)/, args) // Replace with input value
+                        if(cmd.r_type == 'int' && Number.isInteger(Number(newR))) r = newR // int type handling
+                        if(cmd.r_type == 'num' && !Number.isNaN(Number(newR))) r = newR // num type handling
+                    } else if(newR.match(/\(input\:[0-9]+\)/) && args.length > 0) { // If r accepts certain word of input and args are present
+                        newR = newR.replace(/\(input:[0-9]+\)/, args.split(' ')[newR.replace(')','').split(':')[1]]) // Replace with input value
+                        if(cmd.r_type.startsWith('int') && Number.isInteger(Number(newR))) r = newR // int type handling
+                        if(cmd.r_type.startsWith('num') && !Number.isNaN(Number(newR))) r = newR // num type handling
+                    } else if(!newR.match(/\(input\)/) && !newR.match(/\(input\:[0-9]+\)/)) { // If r is just a value
+                        r = newR; // Set r with no worries :)
                     }
                 }
             }
-            // Parse "r" as a number if required
+            // Parse "r" as a number with constraints if required
             if(cmd.r_type == 'int') r = parseInt(r)
             if(cmd.r_type == 'num') r = Number(r)
             if(cmd.r_type == 'int>0') r = Math.max(0, parseInt(r))
@@ -74,10 +79,10 @@ client.on('message', async msg => {
             
             // Execute command
             let img;
-            if(cmd.library == 'jimp') img = await exec(imageUrl, list);
-            if(cmd.library == 'magick') img = await execGM(imageUrl, list);
+            if(cmd.library == 'jimp') img = await exec(imageUrl, list); // Execute with jimp
+            if(cmd.library == 'magick') img = await execGM(imageUrl, list); // Execute with magick
 
-            let extension = await getFormat(imageUrl)
+            let extension = await getFormat(imageUrl) // Get extension of the output image
 
             // Send image
             sendImage(msg, cmd.title, startTime, img, extension, procMsg)
@@ -100,20 +105,22 @@ client.on('message', async msg => {
             procMsg.delete();
         }
     } else if (cmd.type == 'music' && process.env.MUSIC_ENABLED == "true") { // If command is set as music type
-        musicWorker.postMessage({ msgId: msg.id, channelId: msg.channel.id, args, cmd })
+        musicWorker.postMessage({ msgId: msg.id, channelId: msg.channel.id, args, cmd }) // Post message to music worker
     }
 });
 
+// Create music worker
 let musicWorker;
 if(process.env.MUSIC_ENABLED == "true") {
     const { Worker } = require('worker_threads');
 
     const musicWorkerPath = '/modules/music-worker.js'
-    musicWorker = new Worker(__dirname+musicWorkerPath)
+    musicWorker = new Worker(__dirname+musicWorkerPath) // Spawn worker
 }
 
 var path = require('path'); 
 
+// Create web_images directory if required
 if(process.env.WEB_ENABLED == "true") {
     fs.mkdir("web_images", { recursive: true }, (err) => { 
         if (err) { 
@@ -122,50 +129,50 @@ if(process.env.WEB_ENABLED == "true") {
         console.log('Created web_images directory.'); 
     });
 }
-function gracefulShutdown() {
-    client.destroy();
+function gracefulShutdown() { // When the bot is shut down, it does it politely
+    client.destroy(); // Disconnect from Discord
     console.log("Destroyed client")
-    if(process.env.WEB_ENABLED == "true") {
-        fs.rmdir("web_images", { recursive: true }, () => {
+    if(process.env.WEB_ENABLED == "true") { // If using web
+        fs.rmdir("web_images", { recursive: true }, () => { // Remove web_images directory
             console.log('Removed web_images directory.');
-            process.exit();
+            process.exit(); // Exit node process
         })
     } else {
-        process.exit();
+        process.exit(); // Exit node process
     }
 }
 
-if(process.env.WEB_ENABLED == "true") {
+if(process.env.WEB_ENABLED == "true") { // If using web
     const express = require('express')
-    const app = express()
+    const app = express() // Create express server
 
-    // set up rate limiter: maximum of five requests per minute
+    // Set up rate limiter: maximum of 50 requests per minute
     var RateLimit = require('express-rate-limit');
     var limiter = new RateLimit({
         windowMs: 1*60*1000, // 1 minute
         max: 50
     });
 
-    // apply rate limiter to all requests
+    // Apply rate limiter to all requests
     app.use(limiter);
 
-    app.use(express.static("web/public"))
+    app.use(express.static("web/public")) // Static hosting of web/public folder
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname,"web/index.html"))
+        res.sendFile(path.join(__dirname,"web/index.html")) // Main page
     })
 
-    app.get("/images/*", async (req, res) => {
-        let imgPath = path.join(__dirname,"web_images/"+req.path.split("/")[2])
-        if (fs.existsSync(imgPath)) {
-            const r = fs.createReadStream(imgPath)
-            r.pipe(res)
+    app.get("/images/*", async (req, res) => { // Image hosting
+        let imgPath = path.join(__dirname,"web_images/"+req.path.split("/")[2]) // Define supposed path
+        if (fs.existsSync(imgPath)) { // If file exists
+            const r = fs.createReadStream(imgPath) // Stream image to browser
+            r.pipe(res) // Pipe stream
         } else {
-            const r = fs.createReadStream("assets/imageExpired.png")
-            r.pipe(res)
+            const r = fs.createReadStream("assets/imageExpired.png") // Stream "Image expired" to browser
+            r.pipe(res) // Pipe stream
         }
     })
 
-    app.listen(process.env.WEB_PORT, () => {
+    app.listen(process.env.WEB_PORT, () => { // Listen on port
         console.log(`Web host listening at http${process.env.WEB_SECURE == "true" ? "s" : ""}://${process.env.WEB_HOSTNAME}:${process.env.WEB_PORT}`)
     })
 }

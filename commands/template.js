4afac0095cb2b9ce51a86f41a7d269db27467cd2
Barnaby <barnab.why@gmail.com>
2021-02-22 22:44:13 +0000

    Add comments to commands/template.js

--- commands/template.js
+++ commands/template.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
-async function cmdFunc(msg, args, startTime) {}
+async function cmdFunc(msg, args, startTime) {
+  // Command goes here
+}
 
 module.exports = {
   cmdFunc,

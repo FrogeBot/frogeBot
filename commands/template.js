c9bca5d0751136f367ffb13fb8691e01a12b5388
Barnaby <barnab.why@gmail.com>
2021-02-27 10:57:06 +0000

    Update template.js

--- commands/template.js
+++ commands/template.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 
 async function cmdFunc(msg, args, startTime) {}
-async function cmdFuncSlash(client, interaction, args, startTime) {}
 
 module.exports = {
   cmdFunc,
-  cmdFuncSlash,
 };
